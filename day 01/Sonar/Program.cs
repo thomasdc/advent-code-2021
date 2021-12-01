025d5891f29f282b9417a689647a2c48b5b8245b
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2021-12-01 15:39:23 +0100

    day 1 part 1

--- day 01/Sonar/Program.cs
+++ day 01/Sonar/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! 😂");
+﻿using Shouldly;
+using Xunit;
+
+"input.txt".ParseInput<int>().Part1().Print();
+
+public static class Solver
+{
+    public static int Part1(this IReadOnlyCollection<int> depths) => depths
+        .Skip(1)
+        .Select((depth, i) => depth > depths.ElementAt(i))
+        .Count(increased => increased);
+}
+
+public static class Utils
+{
+    public static IReadOnlyCollection<T> ParseInput<T>(this string fileName) =>
+        File.ReadAllLines(fileName).Select(_ => (T)Convert.ChangeType(_, typeof(T))).ToArray();
+
+    public static void Print(this object o) => Console.WriteLine(o);
+}
+
+public class Tests
+{
+    [Fact]
+    public void ValidatePart1Example() => "example.txt".ParseInput<int>().Part1().ShouldBe(7);
+}
