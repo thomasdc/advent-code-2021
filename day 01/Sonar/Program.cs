31a90a6e74829daf094bbf69b135c0af280ace5a
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2021-12-02 20:00:31 +0100

    day 1 part 2

--- day 01/Sonar/Program.cs
+++ day 01/Sonar/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Shouldly;
 using Xunit;
 
-"input.txt".ParseInput<int>().Part1().Print();
+"input.txt".ParseInput<int>().Part2().Print();
 
 public static class Solver
 {
@@ -9,6 +9,12 @@ public static class Solver
         .Skip(1)
         .Select((depth, i) => depth > depths.ElementAt(i))
         .Count(increased => increased);
+
+    public static int Part2(this IReadOnlyCollection<int> depths) => Enumerable.Range(0, depths.Count)
+        .Take(depths.Count - 2)
+        .Select(i => depths.ElementAt(i) + depths.ElementAt(i + 1) + depths.ElementAt(i + 2))
+        .ToArray()
+        .Part1();
 }
 
 public static class Utils
@@ -23,4 +29,7 @@ public class Tests
 {
     [Fact]
     public void ValidatePart1Example() => "example.txt".ParseInput<int>().Part1().ShouldBe(7);
+
+    [Fact]
+    public void ValidatePart2Example() => "example.txt".ParseInput<int>().Part2().ShouldBe(5);
 }
