dd70bcadc5af00ca550b81e4c002a2a8db9d71c5
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2021-12-06 22:45:21 +0100

    day 6 part 2

--- day 06/Fishy/Program.cs
+++ day 06/Fishy/Program.cs
@@ -1,19 +1,22 @@
 ﻿using Shouldly;
 using Xunit;
 
-"input.txt".ParseInput().Part1().Print();
+"input.txt".ParseInput().Part2().Print();
 
 public static class Solver
 {
-    public static int Part1(this int[] timers)
+    public static long Part1(this int[] timers) => timers.Iterate().Skip(79).Take(1).Single().Sum(_ => _.Value);
+    public static long Part2(this int[] timers) => timers.Iterate().Skip(255).Take(1).Single().Sum(_ => _.Value);
+
+    private static IEnumerable<IDictionary<int, long>> Iterate(this int[] timers)
     {
         var groups = timers
             .GroupBy(_ => _)
-            .Select(_ => new { timer = _.Key, numberOfFish = _.Count() })
-            .Union(Enumerable.Range(0, 9).Select(_ => new { timer = _, numberOfFish = 0 }))
+            .Select(_ => new { timer = _.Key, numberOfFish = (long) _.Count() })
+            .Union(Enumerable.Range(0, 9).Select(_ => new { timer = _, numberOfFish = 0L }))
             .GroupBy(_ => _.timer)
             .ToDictionary(_ => _.Key, _ => _.Select(x => x.numberOfFish).Sum());
-        for (var iteration = 1; iteration <= 80; iteration++)
+        for (var iteration = 1;; iteration++)
         {
             var numberOfHatches = groups[0];
             for (var i = 0; i < 8; i++)
@@ -23,9 +26,9 @@ public static class Solver
 
             groups[6] += numberOfHatches;
             groups[8] = numberOfHatches;
+
+            yield return groups;
         }
-        
-        return groups.Sum(_ => _.Value);
     }
 }
 
@@ -41,4 +44,7 @@ public class Tests
 {
     [Fact]
     public void ValidatePart1Example() => "example.txt".ParseInput().Part1().ShouldBe(5934);
+
+    [Fact]
+    public void ValidatePart2Example() => "example.txt".ParseInput().Part2().ShouldBe(26984457539);
 }
