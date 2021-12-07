f5b710ee907c682246c55d246d9e798d807cda5a
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2021-12-07 21:01:40 +0100

    day 7 part 2

--- day 07/Crab/Program.cs
+++ day 07/Crab/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using Shouldly;
 using Xunit;
 
-"input.txt".ParseInput().Part1().Print();
+"input.txt".ParseInput().Part2().Print();
 
 public static class Solver
 {
@@ -12,6 +12,26 @@ public static class Solver
     {
         var median = positions.OrderBy(_ => _).ElementAt(positions.Length / 2);
         return positions.Sum(_ => Math.Abs(_ - median));
+    }
+
+    public static int Part2(this int[] crabs)
+    {
+        var min = crabs.Min();
+        var max = crabs.Max();
+        var totalFuelCost = new int[max - min];
+        var position = min;
+        for (var i = 0; i < totalFuelCost.Length; i++)
+        {
+            totalFuelCost[i] = crabs.Sum(crab =>
+            {
+                var n = Math.Abs(crab - position);
+                return n * (n + 1) / 2;
+            });
+
+            position++;
+        }
+
+        return totalFuelCost.Min();
     }
 }
 
@@ -27,4 +47,7 @@ public class Tests
 {
     [Fact]
     public void ValidatePart1Example() => "example.txt".ParseInput().Part1().ShouldBe(37);
+
+    [Fact]
+    public void ValidatePart2Example() => "example.txt".ParseInput().Part2().ShouldBe(168);
 }
