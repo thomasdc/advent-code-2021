7bac4aa91b103853d0c259b874d3796b46e0dfe4
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2021-12-10 09:50:15 +0100

    day 9 part 2

--- day 09/Heights/Program.cs
+++ day 09/Heights/Program.cs
@@ -5,36 +5,67 @@ using System.Linq;
 using Shouldly;
 using Xunit;
 
-"input.txt".ParseInput().Part1().Print();
+"input.txt".ParseInput().Part2().Print();
 
 public static class Solver
 {
-    public static int Part1(this int[][] heights)
+    private static readonly (int x, int y)[] NeighbourVectors = { (0, -1), (0, 1), (-1, 0), (1, 0) };
+
+    public static int Part1(this int[][] heights) => heights.Lows().Select(_ => heights[_.y][_.x]).Sum(_ => _ + 1);
+
+    public static int Part2(this int[][] heights)
     {
-        var lows = new List<int>();
         var height = heights.Length;
         var width = heights[0].Length;
-        var neighbourVectors = new (int x, int y)[] { (0, -1), (0, 1), (-1, 0), (1, 0) };
+        var basins = new List<(int x, int y)[]>();
+        foreach (var (lowX, lowY) in heights.Lows())
+        {
+            var basin = new List<(int x, int y)> { (lowX, lowY) };
+            var newAdditions = new List<(int x, int y)> { (lowX, lowY) };
+            while (newAdditions.Any())
+            {
+                newAdditions.Clear();
+                foreach (var point in basin)
+                {
+                    newAdditions.AddRange(point.Neighbours(width, height)
+                        .Where(_ => !basin.Contains(_) && !newAdditions.Contains(_))
+                        .Where(_ => heights[_.y][_.x] != 9));
+                }
+
+                basin.AddRange(newAdditions);
+            }
+
+            basins.Add(basin.ToArray());
+        }
+
+        return basins
+            .OrderByDescending(_ => _.Length)
+            .Take(3)
+            .Select(_ => _.Length)
+            .Aggregate(1, (result, size) => result * size);
+    }
+
+    private static IEnumerable<(int x, int y)> Lows(this int[][] heights)
+    {
+        var height = heights.Length;
+        var width = heights[0].Length;
         for (var y = 0; y < height; y++)
         {
             for (var x = 0; x < width; x++)
             {
-                var currentHeight = heights[y][x];
-                var neighbours = neighbourVectors
-                    .Select(_ => (x: x + _.x, y: y + _.y))
-                    .Where(_ => !OutOfBounds(_))
-                    .Select(_ => heights[_.y][_.x])
-                    .ToArray();
-                if (!neighbours.Any(_ => _ <= currentHeight))
+                var neighbours = (x, y).Neighbours(width, height).Select(_ => heights[_.y][_.x]).ToArray();
+                if (!neighbours.Any(_ => _ <= heights[y][x]))
                 {
-                    lows.Add(currentHeight);
+                    yield return (x, y);
                 }
             }
         }
-        
-        bool OutOfBounds((int x, int y) i) => i.x < 0 || i.x >= width || i.y < 0 || i.y >= height;
-        return lows.Sum(_ => _ + 1);
     }
+
+    private static (int x, int y)[] Neighbours(this (int x, int y) point, int width, int height) => NeighbourVectors
+            .Select(_ => (x: point.x + _.x, y: point.y + _.y))
+            .Where(_ => !(_.x < 0 || _.x >= width || _.y < 0 || _.y >= height))
+            .ToArray();
 }
 
 public static class Utils
@@ -49,4 +80,7 @@ public class Tests
 {
     [Fact]
     public void ValidatePart1Example() => "example.txt".ParseInput().Part1().ShouldBe(15);
+
+    [Fact]
+    public void ValidatePart2Example() => "example.txt".ParseInput().Part2().ShouldBe(1134);
 }
