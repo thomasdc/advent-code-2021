69d3a0f273b0f4fe277a10dc282de45d009a3b94
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2021-12-24 09:45:51 +0100

    day 16 part 2

--- day 16/Decoder/Program.cs
+++ day 16/Decoder/Program.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using Xunit;
 
-"input.txt".ParseInput().Part1().Print();
+"input.txt".ParseInput().Part2().Print();
 
 public static class Solver
 {
@@ -11,6 +11,13 @@ public static class Solver
         var bits = new BitEnumerator(transmission.Bits());
         var packet = Packet.Create(bits);
         return packet.VersionSum;
+    }
+
+    public static long Part2(this string transmission)
+    {
+        var bits = new BitEnumerator(transmission.Bits());
+        var packet = Packet.Create(bits);
+        return packet.CalculateValue();
     }
 
     private static IEnumerable<bool> Bits(this string transmission)
@@ -46,12 +53,15 @@ public abstract class Packet
         return packet;
     }
 
+    public abstract long CalculateValue();
     protected abstract void Process(BitEnumerator bits);
 }
 
 public class LiteralValue : Packet
 {
     public long Number { get; private set; }
+
+    public override long CalculateValue() => Number;
 
     protected override void Process(BitEnumerator bits)
     {
@@ -70,9 +80,23 @@ public class LiteralValue : Packet
 public class Operator : Packet
 {
     private readonly List<Packet> _subPackets = new();
-    public IReadOnlyCollection<Packet> SubPackets => _subPackets.AsReadOnly();
 
     public override long VersionSum => Version + _subPackets.Sum(_ => _.VersionSum);
+
+    public override long CalculateValue()
+    {
+        return TypeId switch
+        {
+            0 => _subPackets.Sum(_ => _.CalculateValue()),
+            1 => _subPackets.Aggregate(1L, (acc, _) => acc * _.CalculateValue()),
+            2 => _subPackets.Min(_ => _.CalculateValue()),
+            3 => _subPackets.Max(_ => _.CalculateValue()),
+            5 => _subPackets.ElementAt(0).CalculateValue() > _subPackets.ElementAt(1).CalculateValue() ? 1 : 0,
+            6 => _subPackets.ElementAt(0).CalculateValue() < _subPackets.ElementAt(1).CalculateValue() ? 1 : 0,
+            7 => _subPackets.ElementAt(0).CalculateValue() == _subPackets.ElementAt(1).CalculateValue() ? 1 : 0,
+            _ => throw new NotImplementedException(),
+        };
+    }
 
     protected override void Process(BitEnumerator bits)
     {
@@ -139,4 +163,15 @@ public class Tests
     [InlineData("C0015000016115A2E0802F182340", 23)]
     [InlineData("A0016C880162017C3686B18A3D4780", 31)]
     public void ValidatePart1Examples(string transmission, int versionSum) => transmission.Part1().ShouldBe(versionSum);
+    
+    [Theory]
+    [InlineData("C200B40A82", 3)]
+    [InlineData("04005AC33890", 54)]
+    [InlineData("880086C3E88112", 7)]
+    [InlineData("CE00C43D881120", 9)]
+    [InlineData("D8005AC2A8F0", 1)]
+    [InlineData("F600BC2D8F", 0)]
+    [InlineData("9C005AC2F8F0", 0)]
+    [InlineData("9C0141080250320F1802104A08", 1)]
+    public void ValidatePart2Examples(string transmission, int value) => transmission.Part2().ShouldBe(value);
 }
